382a2c7aad4d13a8cef8f02a78a5418453573391
nura-on <xnamespooferx@gmail.com>
2013-12-15 21:49:58 +0100

    Added GameManager and updated Hero collision
    GameManager handles highscore and Game Over
    Hero has more accurate collision detection now

---
+++ Assets/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+
+    public bool alive = true, showHighscore = false, enableRestart = false;
+
+    public double highscore = 0f;
+
+    private static GameManager _selfRef;
+    Texture2D _tex2d;
+
+    GUIStyle centeredStyle;
+    public Font font;
+
+    // Use this for initialization
+    void Awake()
+    {
+        _selfRef = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!alive)
+        {
+            showHighscore = true;
+
+            StartCoroutine("_HandleRestart");
+            if (Input.GetKeyDown(KeyCode.Space) && enableRestart)
+            {
+                Application.LoadLevel(0);
+            }
+        }
+    }
+
+    IEnumerator _HandleRestart()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+
+        enableRestart = true;
+
+        yield return null;
+    }
+
+
+    void OnGUI()
+    {
+        if (showHighscore)
+        {
+
+            centeredStyle = GUI.skin.GetStyle("Label");
+            centeredStyle.alignment = TextAnchor.UpperCenter;
+            centeredStyle.font = font;
+
+           centeredStyle.fontSize = 12;
+
+            _tex2d = new Texture2D(1, 1);
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height)
+                                , _tex2d
+                                , ScaleMode.StretchToFill
+                                , true
+                                , 1.0F);
+
+            GUI.Label(new Rect((Screen.width / 2) - 400, Screen.height / 2 - 250, 800, 500), "<color=red><size=100>YOU LOST!</size></color>", centeredStyle);
+            GUI.Label(new Rect((Screen.width / 2) - 400, Screen.height / 2 - 92, 800, 500), "<color=red><size=35>ran " + highscore + "m before you</size></color>", centeredStyle);
+            GUI.Label(new Rect((Screen.width / 2) - 400, Screen.height / 2 + 120, 800, 500), "<color=white><size=15>Press SPACE to play again</size></color>", centeredStyle);
+
+
+        }
+    }
+
+
+    #region getter setter
+    public static bool Alive
+    {
+        get { return _selfRef.alive; }
+        set { _selfRef.alive = value; }
+    }
+
+    public static double Highscore
+    {
+        get { return _selfRef.highscore; }
+        set { _selfRef.highscore = value; }
+    }
+    #endregion
+}
--- Assets/Movement_Hero.cs
+++ Assets/Movement_Hero.cs
@@ -3,130 +3,136 @@ using System.Collections;
 
 public class Movement_Hero : MonoBehaviour
 {
-	private Vector2[] RayCheckStartPoints = new Vector2[2];
-	private bool DaOneIsPerformed = true;
-	private int MovementSpeed = 5;
-	private int GravitySpeed = -1;
-	private int JumpStepHeightMax = 24;
-	private int JumpIntervalMultiplicator = 2;
-	public bool Grounded = false;
-	private bool Alive = true;
+    private Vector2[] RayCheckStartPoints = new Vector2[2];
+    private bool DaOneIsPerformed = true;
+    private int MovementSpeed = 5;
+    private int GravitySpeed = -1;
+    private int JumpStepHeightMax = 24;
+    private int JumpIntervalMultiplicator = 2;
+    public bool Grounded = false;
     private bool keyPressed = false;
 
-	private Ground_Manager MyGroundManager;
-	private GameObject CurrentGround;
-	// Use this for initialization
-	void Start ()
-	{
-		MyGroundManager = GameObject.Find ("Ground_Manager").GetComponent<Ground_Manager>();
-	}
-	void Update ()
-	{
+    private Vector2 MaxMoveMovementDistance;
+    private RaycastHit2D RayCastHit;
+    private int CurrentGravity = 1;
+
+    private long stepCounter = 0; // for highscore
+
+    private Ground_Manager MyGroundManager;
+    private GameObject CurrentGround;
+    // Use this for initialization
+    void Start()
+    {
+        MyGroundManager = GameObject.Find("GameManager").GetComponent<Ground_Manager>();
+    }
+    void Update()
+    {
         keyPressed = Input.GetKeyDown(KeyCode.Space);
-		if (DaOneIsPerformed)
-		{
-            if (keyPressed)
-			{
-				//Debug.Log("UPDATE: PosX: " + (transform.position.x + 8) + " | " + "Grounded: " + Grounded);
-				DaOneIsPerformed = false;
-			}
-		}
-        if (!Alive) {
+        if (DaOneIsPerformed && GameManager.Alive)
+        {
             if (keyPressed)
             {
-                Application.LoadLevel(0);  
+                //Debug.Log("UPDATE: PosX: " + (transform.position.x + 8) + " | " + "Grounded: " + Grounded);
+                DaOneIsPerformed = false;
             }
         }
-	}
-	// Update is called once per frame
-	void FixedUpdate ()
-	{
-		if (Alive)
-		{
-			if (!DaOneIsPerformed)
-			{
-				//Debug.Log("JUMPED: PosX: " + (transform.position.x + 8) + " | " + "Grounded: " + Grounded);
-				if (Grounded)
-				{
-					StartCoroutine (PerformJump());
-				}
-				DaOneIsPerformed = true;
-			}
-			for (int i = 0; i < MovementSpeed; i++)
-			{
-				transform.Translate (1, 0, 0);
-			}
-			PerformGravity ();
-		}
-	}
-	IEnumerator PerformJump ()
-	{
-		int CurrentJumpInterval = JumpStepHeightMax;
-		while (CurrentJumpInterval > 0)
-		{
-			transform.Translate(new Vector3(0, CurrentJumpInterval, 0));
-			CurrentJumpInterval -= JumpIntervalMultiplicator;
-			yield return new WaitForFixedUpdate ();
-		}
-	}
-	private Vector2 MaxMoveMovementDistance;
-	private RaycastHit2D RayCastHit;
-	private int CurrentGravity = 1;
-	void PerformGravity ()
-	{
-		RayCheckStartPoints = new Vector2[2];
-		RayCheckStartPoints [0] = new Vector2 (transform.position.x + 0, transform.position.y);
-		RayCheckStartPoints [1] = new Vector2 (transform.position.x + 16, transform.position.y);
-		int PerformDistance = Mathf.Abs(CurrentGravity);
-		for (int i = 0; i < RayCheckStartPoints.Length; i++)
-		{
-			//Debug.DrawLine(RayCheckStartPoints[i], new Vector3(RayCheckStartPoints[i].x, 0, 0));
-			RayCastHit = Physics2D.Raycast(RayCheckStartPoints[i], new Vector2(0, -1), Mathf.Abs(CurrentGravity), 1 <<31);
-			if (RayCastHit)
-			{
-				if (Mathf.RoundToInt(Mathf.Abs(RayCheckStartPoints[i].y - RayCastHit.point.y)) == 0)
-				{
-					if (transform.position.y < RayCastHit.collider.transform.position.y)
-					{
-						Debug.Log("DEAD MOFO");
-						Die();
-						return;
-					}
-					CurrentGravity = 1;
-					Grounded = true;
-					return;
-				}
-				else
-				{
-					PerformDistance = Mathf.RoundToInt(Mathf.Abs(RayCheckStartPoints[i].y - RayCastHit.point.y));
-					transform.Translate(0, -PerformDistance, 0);
-					Grounded = true;
-					return;
-				}
-			}
-		}
-		Grounded = false;
-		transform.Translate(0, -PerformDistance, 0);
-		CurrentGravity += GravitySpeed;
-	}
-	void OnTriggerEnter2D (Collider2D col)
-	{
-		Debug.Log("Triggered!: " + Grounded);
-		if (col.gameObject.layer == 31)
-		{
-			if (CurrentGround != col.gameObject)
-			{
-				MyGroundManager.OrderNextGround();
-				CurrentGround = col.gameObject;
-			}
-			if (!Grounded)
-			{
-				Die();
-			}
-		}
-	}
-	void Die()
-	{
-		Alive = false;
-	}
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (GameManager.Alive)
+        {
+            if (!DaOneIsPerformed)
+            {
+                //Debug.Log("JUMPED: PosX: " + (transform.position.x + 8) + " | " + "Grounded: " + Grounded);
+                if (Grounded)
+                {
+                    StartCoroutine(PerformJump());
+                }
+                DaOneIsPerformed = true;
+            }
+            for (int i = 0; i < MovementSpeed; i++)
+            {
+                transform.Translate(1, 0, 0);
+
+                if (stepCounter < 12) stepCounter++;
+                else
+                {
+                    stepCounter = 0;
+                    GameManager.Highscore += 0.5;
+                }
+            }
+            PerformGravity();
+        }
+    }
+    IEnumerator PerformJump()
+    {
+        int CurrentJumpInterval = JumpStepHeightMax;
+        while (CurrentJumpInterval > 0)
+        {
+            transform.Translate(new Vector3(0, CurrentJumpInterval, 0));
+            CurrentJumpInterval -= JumpIntervalMultiplicator;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    // edit nyrrrr: this could probably have been made easier if we just added a groundcheck object to the hero and used proper collision detection provided by unity
+    void PerformGravity()
+    {
+        RayCheckStartPoints = new Vector2[2];
+        // edit nyrrrr: this might be wrong (TODO ?)
+        RayCheckStartPoints[0] = new Vector2(transform.position.x+1, transform.position.y);
+        RayCheckStartPoints[1] = new Vector2(transform.position.x + ((BoxCollider2D)transform.collider2D).size.x+1, transform.position.y);
+        int PerformDistance = Mathf.Abs(CurrentGravity);
+        for (int i = 0; i < RayCheckStartPoints.Length; i++)
+        {
+            //Debug.DrawLine(RayCheckStartPoints[i], new Vector3(RayCheckStartPoints[i].x, 0, 0));
+            RayCastHit = Physics2D.Raycast(RayCheckStartPoints[i], new Vector2(0, -1), Mathf.Abs(CurrentGravity), 1 << 31);
+            if (RayCastHit)
+            {
+                if (Mathf.RoundToInt(Mathf.Abs(RayCheckStartPoints[i].y - RayCastHit.point.y)) == 0)
+                {
+                    if (transform.position.y < RayCastHit.collider.transform.position.y)
+                    {
+                        //Debug.Log("DEAD MOFO");
+                        Die();
+                        return;
+                    }
+                    CurrentGravity = 1;
+                    Grounded = true;
+                    return;
+                }
+                else
+                {
+                    PerformDistance = Mathf.RoundToInt(Mathf.Abs(RayCheckStartPoints[i].y - RayCastHit.point.y));
+                    transform.Translate(0, -PerformDistance, 0);
+                    Grounded = true;
+                    return;
+                }
+            }
+        }
+        Grounded = false;
+        transform.Translate(0, -PerformDistance, 0);
+        CurrentGravity += GravitySpeed;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("Triggered!: " + Grounded);
+        if (col.gameObject.layer == 31)
+        {
+            if (CurrentGround != col.gameObject)
+            {
+                MyGroundManager.OrderNextGround();
+                CurrentGround = col.gameObject;
+            }
+            if (!Grounded)
+            {
+                Die();
+            }
+        }
+    }
+    void Die()
+    {
+        GameManager.Alive = false;
+    }
 }
